194c691f319c6dd5d3de2d37c83d2f3b21f48caf
TheVenYear <TheVenYear@gmail.com>
2021-05-20 21:36:15 +0300

    cors deleted

--- src/app.js
+++ src/app.js
@@ -8,14 +8,12 @@ import fileUpload from 'express-fileupload';
 import routes from './routes';
 import errorHandler from './middlewares/error-handler';
 import swagger from './config/swagger';
-import corsSettings from './config/cors-settings';
 
 const app = express();
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
 // Before middlewares
-app.use(cors(corsSettings));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
