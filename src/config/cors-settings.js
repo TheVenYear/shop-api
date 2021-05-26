cf12da798407507468c33ebb9f511f4262a2844e
TheVenYear <TheVenYear@gmail.com>
2021-05-26 23:53:00 +0300

    cors fix2

--- src/config/cors-settings.js
+++ src/config/cors-settings.js
@@ -1,8 +1,12 @@
-const whitelist = ['http://localhost:3000/', 'https://glendi123.github.io/'];
+const whitelist = [
+  'http://localhost:3000/',
+  'https://glendi123.github.io/',
+  '*',
+];
 
 const corsSettings = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Заблокировано политикой CORS'));
