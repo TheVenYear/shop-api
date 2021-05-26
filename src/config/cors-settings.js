e7f29d54978759c5da3161d45a5a84998f42d62c
TheVenYear <TheVenYear@gmail.com>
2021-05-27 00:18:08 +0300

    cors fix 4

--- src/config/cors-settings.js
+++ src/config/cors-settings.js
@@ -1,13 +1,8 @@
-const whitelist = ['http://localhost:3000/', 'https://glendi123.github.io/'];
+import config from '.';
 
 const corsSettings = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Заблокировано политикой CORS'));
-    }
-  },
+  origin:
+    config.NODE_ENV === 'production' ? config.HOST : 'http://localhost:8000/',
   credentials: true,
   preflightContinue: true,
 };
